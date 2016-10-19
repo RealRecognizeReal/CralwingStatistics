ed6ce1b4452a853815856a5827f07d7a13861b2a
Taewoo Kim <xodn4195@gmail.com>
2016-10-19 15:32:54 +0900

    Add graph for statistics

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const
     router = express.Router(),
     MongoClient = require('mongodb').MongoClient,
     {mongodb}  = require('../config'),
+    Promise = require('bluebird'),
     assert = require('assert');
 
 const mongoUrl = `mongodb://${mongodb.host}:${mongodb.port}/${mongodb.database}`;
@@ -29,9 +30,13 @@ router.get('/', function (req, res, next) {
                     if(err) return next(err);
 
                     page.find({formulasNumber: {$gt: 0}}).sort({_id: -1}).limit(10).toArray(function(err, recentPages) {
-                        console.log(allPagesNumber, formulaPagesNumber, formulasNumber, recentPages);
+                        //console.log(allPagesNumber, formulaPagesNumber, formulasNumber, recentPages);
 
-                        res.render('index', {title: 'Express', allPagesNumber, formulaPagesNumber, formulasNumber, recentPages});
+                        res.render('index',
+                            {
+                                title: 'alan statistics',
+                                allPagesNumber, formulaPagesNumber, formulasNumber, recentPages
+                            });
 
                         db.close();
                     })
@@ -42,5 +47,21 @@ router.get('/', function (req, res, next) {
         })
     })
 });
+
+router.get('/api/stat', Promise.coroutine(function*(req, res, next) {
+    let db = yield MongoClient.connect(mongoUrl);
+
+    let stat = db.collection('stat');
+
+    let data = yield stat.find().sort({createdAt: -1}).limit(1000).toArray();
+
+    db.close();
+
+    res.send(data);
+}));
+
+router.get('/stat', Promise.coroutine(function*(req, res, next) {
+    res.render('stat', {title: 'alan perf statistics'});
+}));
 
 module.exports = router;
